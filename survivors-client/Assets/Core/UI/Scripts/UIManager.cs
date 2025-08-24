c9be9ae611a3eb39d26a1289d81eb3cf1768d575
Koi <sviridalos@gmail.com>
2025-08-24 22:55:08 +0200

    added namespace to ui manager

--- survivors-client/Assets/Core/UI/Scripts/UIManager.cs
+++ survivors-client/Assets/Core/UI/Scripts/UIManager.cs
@@ -1,16 +1,8 @@
 using UnityEngine;
 
-public class UIManager : MonoBehaviour
+namespace Survivors.Core.UI
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class UIManager : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
