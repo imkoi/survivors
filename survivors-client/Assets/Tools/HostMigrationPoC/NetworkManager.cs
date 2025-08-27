1923618dba9eff22a1730c9abac5e3f15a08416e
imkoi <sviridalos@gmail.com>
2025-08-27 19:32:06 +0200

    fixed network manager in both pocs

--- survivors-client/Assets/Tools/HostMigrationPoC/NetworkManager.cs
+++ survivors-client/Assets/Tools/HostMigrationPoC/NetworkManager.cs
@@ -12,17 +12,16 @@ public class NetworkManager : MonoBehaviour
     {
         _networkRunner.StartGame(new StartGameArgs
         {
-            SessionName = "Koi Session",
+            GameMode = GameMode.Shared,
+            //SessionName = "Koi Session",
             SessionProperties = new Dictionary<string, SessionProperty>(),
-            CustomLobbyName = "Koi Lobby",
-            EnableClientSessionCreation = true,
+            //CustomLobbyName = "Koi Lobby",
+            //EnableClientSessionCreation = true,
             PlayerCount = 32,
             IsOpen = true,
             IsVisible = true,
-            MatchmakingMode = MatchmakingMode.SerialMatching,
+            //MatchmakingMode = MatchmakingMode.SerialMatching,
         });
-        
-        
     }
 
     // Update is called once per frame
